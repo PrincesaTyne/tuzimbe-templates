9c9df146609d76b6c778a488e59d2d7b631b6105
PrincesaTyne <justinejjingo@gmail.com>
2021-11-02 03:18:06 +0300

    add age and sex fields

--- src/js/contractors.js
+++ src/js/contractors.js
@@ -1,12 +1,12 @@
 export const contractorsList = document.querySelector("tbody");
 
 const contractors = [
-  { id: "1", name: "John Doe", email: "john.doe@tuzimbe.com", role: "Manager" },
-  { id: "2", name: "Blue Sky", email: "blue.sky@tuzimbe.com", role: "Porter" },
-  { id: "3", name: "Green Beans", email: "green.beans@tuzimbe.com", role: "Carpenter" },
-  { id: "4", name: "Apple Tree", email: "apple.tree@tuzibe.com", role: "Builder" },
-  { id: "5", name: "Qwerty Asdfg", email: "qwerty@tuzimbe.com", role: "Carpenter" },
-  { id: "6", name: "May Flower", email: "may.flower@tuzimbe.com", role: "Porter" },
+  { id: "1", name: "John Doe", email: "john.doe@tuzimbe.com", age: "45", sex: "Female", role: "Manager" },
+  { id: "2", name: "Blue Sky", email: "blue.sky@tuzimbe.com", age: "18", sex: "Male", role: "Porter" },
+  { id: "3", name: "Green Beans", email: "green.beans@tuzimbe.com", age: "28", sex: "Female", role: "Carpenter" },
+  { id: "4", name: "Apple Tree", email: "apple.tree@tuzibe.com", age: "30", sex: "Male", role: "Builder" },
+  { id: "5", name: "Qwerty Asdfg", email: "qwerty@tuzimbe.com", age: "25", sex: "Female", role: "Carpenter" },
+  { id: "6", name: "May Flower", email: "may.flower@tuzimbe.com", age: "34", sex: "Male", role: "Porter" },
 
 ];
 
@@ -15,6 +15,8 @@ contractors.forEach(contractor => {
       <td>${ contractor.id }</td>
       <td>${ contractor.name }</td>
       <td>${ contractor.email }</td>
+      <td>${ contractor.age }</td>
+      <td>${ contractor.sex }</td>
       <td>${ contractor.role }</td>
       <td>
         <button class="btn btn-outline-success">View Details</button>
